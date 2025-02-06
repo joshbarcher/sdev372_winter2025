b60464af952293db3ed4a17d339d161ed94d7f99
Josh Archer <jarcher@greenriver.edu>
2025-02-06 14:52:16 -0800

    Added some starting files for a front-end app.

--- web-api/src/server.js
+++ web-api/src/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import router from './routers/gamesRouter.js';
+import cors from 'cors';
 
 const app = express();
+
+//allow cross origin script requests
+app.use(cors({
+    origin: "*"
+}));
+
+//serve up static files
+app.use(express.static("./public"));
 
 //parse JSON payloads
 app.use(express.json());
