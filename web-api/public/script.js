8bc0f7292bfd07e5289afea5ad36b7c310e92c10
Josh Archer <jarcher@greenriver.edu>
2025-02-11 14:22:56 -0800

    Added examples of GET and POST with fetch().

--- web-api/public/script.js
+++ web-api/public/script.js
@@ -11,12 +11,41 @@ window.onload = async () => {
     loadUI(json.data);
 
     const button = document.getElementById("gameSubmit");
-    button.onclick = addGame;
+    button.onclick = gameSubmit;
 }
 
-function addGame(event) {
+async function gameSubmit(event) {
     //prevent the default behavior of this event (form submission)
     event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const genre = document.getElementById("genre").value;
+
+    const newGame = {
+        title,
+        genre,
+        developer: "CD Project Red",
+        rating: 90,
+        release_date: "2024-08-20"
+    }
+
+    //make a post request to the server to save the data
+    const result = await fetch("/api/v1/games", {
+        method: "post",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newGame)
+    })
+    console.log(result);
+
+    const gameAdded = await result.json();
+    console.log(gameAdded);
+
+    //update our DOM
+    const main = document.querySelector("#games");
+    addGame(newGame, main);
 }
 
 function loadUI(games) {
