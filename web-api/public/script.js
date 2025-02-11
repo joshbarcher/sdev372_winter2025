2ddb497b4efe9eef2e6c1d03e48c707e2f675dd2
Josh Archer <jarcher@greenriver.edu>
2025-02-11 13:53:26 -0800

    Added changes.

--- web-api/public/script.js
+++ web-api/public/script.js
@@ -8,4 +8,48 @@ window.onload = async () => {
     const response = await fetch(uri, config);
     const json = await response.json();
     console.log(json);
+    loadUI(json.data);
+
+    const button = document.getElementById("gameSubmit");
+    button.onclick = addGame;
+}
+
+function addGame(event) {
+    //prevent the default behavior of this event (form submission)
+    event.preventDefault();
+}
+
+function loadUI(games) {
+    const main = document.querySelector("#games");
+
+    for (const game of games) {
+        addGame(game, main);
+    }
+}
+
+function addGame(game, main) {
+    //create DOM elements
+    const section = document.createElement("section");
+    const h2 = document.createElement("h2");
+    const ul = document.createElement("ul");
+
+    //add properties
+    h2.innerHTML = game.title;
+
+    const details = [game.genre, game.developer, game.rating, game.release_date];
+
+    for (const detail of details) {
+        addGameDetail(ul, detail);
+    }
+
+    //nest elements
+    section.appendChild(h2);
+    section.appendChild(ul);
+    main.appendChild(section);
+}
+
+function addGameDetail(ul, detail) {
+    const li = document.createElement("li");
+    li.innerHTML = detail;
+    ul.appendChild(li);
 }
